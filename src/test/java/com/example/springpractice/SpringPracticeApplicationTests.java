176c0567800f5f45155a791eb29a048977e5d3c4
seonghun127 <seonghun127@gmail.com>
2020-03-29 23:26:15 +0900

    refactor: 의존성 수정

--- src/test/java/com/example/springpractice/SpringPracticeApplicationTests.java
+++ src/test/java/com/example/springpractice/SpringPracticeApplicationTests.java
@@ -3,14 +3,11 @@ package com.example.springpractice;
 import com.example.springpractice.redis.cache.MemberCacheStore;
 import com.example.springpractice.redis.entity.Member;
 import com.example.springpractice.redis.entity.MemberRepository;
-import com.google.common.base.Stopwatch;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,31 +37,4 @@ class SpringPracticeApplicationTests {
 		// 3. Then
 		assertThat(memberCacheStore.get("123")).isEqualTo(newMember);
 	}
-
-	@Test
-	void 캐시_조회_시간() {
-		// 1. Given
-		Member member = Member.builder()
-				.memberNo("123")
-				.count(100)
-				.build();
-		Member newMember = memberRepository.save(member);
-		memberCacheStore.set(newMember.getMemberNo(), newMember);
-
-		// 2. When
-		Stopwatch stopwatch = Stopwatch.createStarted();
-		memberRepository.findById(newMember.getId());
-		long dbTime = stopwatch.stop().elapsed(TimeUnit.MICROSECONDS);
-
-		stopwatch.reset();
-		stopwatch.start();
-		memberRepository.findById(newMember.getId());
-		long cacheTime = stopwatch.stop().elapsed(TimeUnit.MICROSECONDS);
-
-		// 3. Then
-		assertThat(dbTime > cacheTime).isTrue();
-		log.info("time retrieving data with DB : {}", dbTime);
-		log.info("time retrieving data with cache : {}", cacheTime);
-	}
-
 }
