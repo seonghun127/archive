72e4e0844f01c16800598826ace8d190b7e8a6d9
seonghun127 <seonghun127@gmail.com>
2020-03-31 19:55:03 +0900

    feat: 테스트 코드 추가

--- src/test/java/com/example/springpractice/SpringPracticeApplicationTests.java
+++ src/test/java/com/example/springpractice/SpringPracticeApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.springpractice;
 
+import com.example.springpractice.entity.EcoMember;
+import com.example.springpractice.repository.EcoMemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private EcoMemberRepository ecoMemberRepository;
 
+	@Test
+	void 유니크_제약조건_테스트() {
+		// 1. Given
+		String memberNo = "123";
+		EcoMember ecoMember1 = EcoMember.builder()
+				.memberNo(memberNo)
+				.count(123)
+				.build();
+		EcoMember ecoMember2 = EcoMember.builder()
+				.memberNo(memberNo)
+				.count(123)
+				.build();
+
+		// 2. When
+		ecoMemberRepository.save(ecoMember1);
+		ecoMemberRepository.save(ecoMember2);
+
+		// 3. Then
+	}
 }
