d9eceee2c031c4414c680bd849f148ae215d9dc6
seonghun127 <seonghun127@gmail.com>
2020-04-09 21:31:27 +0900

    chore: 기존 디렉토리 삭제

--- src/main/java/com/example/springpractice/repository/EcoMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springpractice.repository;
-
-import com.example.springpractice.entity.EcoMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EcoMemberRepository extends JpaRepository<EcoMember, Long> {
-}
